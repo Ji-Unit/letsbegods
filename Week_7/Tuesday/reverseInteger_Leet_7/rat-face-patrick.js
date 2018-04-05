a1489e66a6ebea7efe48cb8cba297b77303ed8be
Patrick Truong <ptruong@zipwhip.com>
2018-04-04 18:43:56 -0700

    update pt

--- Week_7/Tuesday/reverseInteger_Leet_7/rat-face-patrick.js
+++ Week_7/Tuesday/reverseInteger_Leet_7/rat-face-patrick.js
@@ -2,15 +2,15 @@
  * @param {number} x
  * @return {number}
  */
+const reverseAndConvert = number => number.toString().split('').reverse().join('');
+
 const reverse = x => {
-  let numToChange = x;
-  const max32BitInteger = 2147483647; // thanks nick
+    let numToChange = x;
+    const max32BitInteger = 2147483647; // thanks nick
 
-  const isPositive = numToChange > 0;
-  const positiveNumber = isPositive ? numToChange : numToChange *= -1;
-  const revseredNumString = positiveNumber.toString().split('').reverse().join('');
-  const revseredNumber = +revseredNumString;
+    const isPositive = numToChange > 0;
+    const revseredNumber = isPositive ? +reverseAndConvert(x) : +reverseAndConvert(x *= -1)
 
-  if (revseredNumber > max32BitInteger) return 0;
-  return isPositive ? revseredNumber : revseredNumber * -1;
+    if (revseredNumber > max32BitInteger) return 0;
+    return isPositive ? revseredNumber : revseredNumber * -1;
 };
