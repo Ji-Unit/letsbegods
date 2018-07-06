e96c3ee9609ed0cae12ae810719db78feb6ea5b1
Nicholas Ko <mailnickko@gmail.com>
2018-07-05 21:51:55 -0700

    add array algos

---
+++ Series_2/arrays/Nick/001_twoSum.js
@@ -0,0 +1,24 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+const twoSum = (nums, target) => {
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]] !== undefined) {
+      return [hash[num[i]], i];
+    }
+    let diff = target - nums[i];
+    hash[diff] = i;
+  }
+  return [];
+};
